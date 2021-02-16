595499d944d305f027a3186fa5dd8f11f192b974
Praveenrtp <46226861+Praveenrtp@users.noreply.github.com>
2021-02-16 00:10:37 -0500

    Initial commit

---
+++ src/main/scala/org/spark/PopularHashtags.scala
@@ -0,0 +1,51 @@
+package org.spark
+
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.twitter._
+
+object PopularHashtags {
+
+  def setupTwitter(): Unit = {
+    import scala.io.Source
+
+    val lines = Source.fromFile("data/twitter.txt")
+    for (line <- lines.getLines) {
+      val fields = line.split(" ")
+      if (fields.length == 2) {
+        System.setProperty("twitter4j.oauth." + fields(0), fields(1))
+      }
+    }
+    lines.close()
+  }
+
+  def setupLogging(): Unit = {
+    import org.apache.log4j.{Level, Logger}
+    val rootLogger = Logger.getRootLogger
+    rootLogger.setLevel(Level.ERROR)
+  }
+
+
+
+  /** Our main function where the action happens */
+  def main(args: Array[String]) {
+
+    setupTwitter()
+
+    val sc = new StreamingContext("local[*]", "PopularHashtags", Seconds(1))
+
+    setupLogging()
+
+    val tweets = TwitterUtils.createStream(ssc, None)
+    val statuses = tweets.map(status => status.getText)
+    val tweetWords = statuses.flatMap(tweetText => tweetText.split(" "))
+    val hashtags = tweetWords.filter(word => word.startsWith("#"))
+    val hashtagKeyValues = hashtags.map(hashtag => (hashtag, 1))
+    val hashtagCounts = hashtagKeyValues.reduceByKeyAndWindow( (x,y) => x + y, (x,y) => x - y, Seconds(300), Seconds(1))
+    val sortedResults = hashtagCounts.transform(rdd => rdd.sortBy(x => x._2, ascending = false))
+
+    sortedResults.print
+    ssc.checkpoint("C:/checkpoint/")
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
